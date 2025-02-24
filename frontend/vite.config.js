09afe7aceb3b578bd1287b8543449b39593aee21
Leandro Laurenzette <140437550+Leandro-Plan@users.noreply.github.com>
2025-02-24 19:35:10 -0300

    nao funciona pelo celular

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,13 @@ import viteReact from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [viteReact()],
   server: {
+    host: '0.0.0.0',
+    headers: {
+      'access-control-allow-origin': '*',
+      'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'access-control-allow-headers': 'Content-Type, Authorization',
+      'Cross-Origin-Opener-Policy': 'same-origin-allow-popups'
+    },
     port: 3000,
     proxy: {
 
