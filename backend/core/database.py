fe9e612807f3e54495c730be8cba291f7af696ac
Leandro Laurenzette <leandrolaurenzette@gmail.com>
2025-04-16 14:20:18 -0300

    feat(database): implement Database class for user management with table creation and registration

---
+++ backend/core/database.py
@@ -0,0 +1,141 @@
+import logging
+import textwrap
+from typing import Any, Dict
+
+from fastapi import HTTPException, status
+
+from backend.core.auth import Auth
+from backend.models import Connection
+
+logger = logging.getLogger(__name__)
+
+
+class Database:
+    """
+    Database class to handle database-related tasks.
+    This class is a singleton to ensure that only one instance is created.
+    """
+
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, "_instance"):
+            cls._instance = super(Database, cls).__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        if not hasattr(self, "_initialized"):
+            self._initialized = True
+
+    def create_table(self) -> Dict[str, Any]:
+        """
+        Create or verify the existence of the 'users' table in the database.
+        This function ensures that the table has the necessary columns and constraints
+
+        Returns:
+            Dict: Result of the operation, indicating success or failure.
+        """
+        sql_query = textwrap.dedent(
+            """\
+            CREATE TABLE IF NOT EXISTS users (
+                id SERIAL PRIMARY KEY,
+                email VARCHAR(255) UNIQUE NOT NULL,
+                password_hash VARCHAR(512) NOT NULL,
+                is_active BOOLEAN DEFAULT TRUE,
+                created_at TIMESTAMP DEFAULT NOW(),
+                updated_at TIMESTAMP DEFAULT NOW(),
+                
+            );
+            
+            -- Create trigger to update the updated_at column on row update
+            CREATE OR REPLACE FUNCTION update_modified_column()
+            RETURNS TRIGGER AS $$
+            BEGIN
+                NEW.updated_at = NOW();
+                RETURN NEW;
+            END;
+            $$ LANGUAGE plpgsql;
+            
+            DROP TRIGGER IF EXISTS update_users_updated_at ON users;
+            CREATE TRIGGER update_users_updated_at
+            BEFORE UPDATE ON users
+            FOR EACH ROW
+            EXECUTE PROCEDURE update_modified_column();
+        """
+        )
+
+        try:
+            with Connection() as conn:
+                conn.execute(sql_query)
+
+            logger.info("Table created/verified successfully")
+            return {
+                "success": True,
+                "message": "Table users created/verified with success",
+            }
+
+        except ValueError as ve:
+            logger.error(f"Value error: {str(ve)}")
+            raise HTTPException(
+                status_code=500, detail="Internal server error during database setup"
+            )
+        except Exception as e:
+            logger.exception("Critical error creating database table")
+            raise HTTPException(
+                status_code=500, detail="Internal server error during database setup"
+            ) from e
+
+    def get_user_from_db(self, email: str) -> dict:
+        """
+        Fetch user details from the database.
+        """
+        try:
+            logger.info(f"Fetching user with email: {email}")
+            with Connection() as conn:
+                result = conn.execute(
+                    "SELECT email, password_hash, is_active FROM postgres.public.users WHERE email = %s",
+                    (email,),
+                )
+                if result:
+                    data_user = result[0]
+                    logger.info(f"User found: {data_user[0]}")
+                    return {
+                        "success": True,
+                        "email": data_user[0],
+                        "password_hash": data_user[1],
+                        "is_active": data_user[2],
+                    }
+                else:
+                    logger.info(f"No user found with email: {email}")
+                    return {
+                        "success": False,
+                        "message": "User not found",
+                    }
+        except Exception as e:
+            logger.error(f"Error fetching user from database: {e}")
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Error on consulting user in database",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+
+        finally:
+            logger.info("Finished fetching user from database")
+
+    def user_register(self, email: str, password: str) -> dict:
+        """
+        Register a new user in the database.
+        """
+        auth = Auth()
+        hashed_password = auth.get_password_hash(password)
+        sql_query = (
+            "INSERT INTO users (email, password_hash) VALUES (%s, %s) RETURNING id"
+        )
+        try:
+            with Connection() as conn:
+                conn.execute(sql_query, (email, hashed_password))
+                return {"success": True, "message": "User registered successfully"}
+        except Exception as e:
+            logger.error(f"Error registering user: {e}")
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Internal server error",
+            )
